77cb06134e32d2d409f4f541118660375b463c53
Tobias Briones <tobiasbriones.dev@gmail.com>
2024-01-25 22:26:13 -0600

    Add eslint support to react and vite

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,57 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    plugins: [
+        "@typescript-eslint",
+        "react",
+        "react-hooks",
     ],
-  },
-}
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended-type-checked",
+        "plugin:@typescript-eslint/stylistic-type-checked",
+        "plugin:react/recommended",
+        "plugin:react/jsx-runtime",
+        "plugin:react-hooks/recommended",
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: "es2022",
+        sourceType: "module",
+        project: [ "tsconfig.json", "tsconfig.node.json" ],
+        tsconfigRootDir: __dirname,
+        ecmaFeatures: { jsx: true },
+    },
+    env: { browser: true, es2022: true, },
+    settings: {
+        react: {
+            version: "detect",
+        },
+    },
+    rules: {
+        indent: [
+            "error",
+            4,
+        ],
+        "linebreak-style": [
+            "error",
+            "unix",
+        ],
+        quotes: [
+            "error",
+            "double",
+        ],
+        semi: [
+            "error",
+            "always",
+        ],
+        "react/jsx-uses-react": "off",
+        "react/react-in-jsx-scope": "off",
+    },
+    ignorePatterns: [ ".eslintrc.cjs" ],
+    overrides: [
+        {
+            env: { node: true, },
+            files: [ ".eslintrc.{js,cjs}", ],
+            parserOptions: { sourceType: "script", },
+        },
+    ],
+};
