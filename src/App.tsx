c3dfa7d24838daa69b8422640ed20894a0c93211
Tobias Briones <tobiasbriones.dev@gmail.com>
2024-01-27 22:16:12 -0600

    Replace dummy icons with app icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import mswLogo from "./assets/msw.svg";
+import mathLogo from "/math.svg";
 import "./App.css";
 
 function App() {
@@ -9,22 +9,22 @@ function App() {
     return (
         <>
             <div>
-                <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+                <a href="#" target="_blank" rel="noreferrer">
                     <img
-                        src={viteLogo}
+                        src={mathLogo}
                         className="logo"
                         alt="Vite logo"
                     />
                 </a>
                 <a href="https://react.dev" target="_blank" rel="noreferrer">
                     <img
-                        src={reactLogo}
+                        src={mswLogo}
                         className="logo react"
                         alt="React logo"
                     />
                 </a>
             </div>
-            <h1>Vite + React</h1>
+            <h1>Math</h1>
             <div className="card">
                 <button onClick={() => setCount((count) => count + 1)}>
                     count is {count}
