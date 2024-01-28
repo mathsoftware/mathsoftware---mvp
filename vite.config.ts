38982d99dbda41028778dd6b17ac821efb33ac01
Tobias Briones <tobiasbriones.dev@gmail.com>
2024-01-28 17:35:40 -0600

    Add @ts-expect-error message to dev dep
    The dependency seems to have a package.json discrepancy, so TS is not considering its types.

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+// @ts-expect-error This dev dep must be fixed to support its types correctly
 import eslintPlugin from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
