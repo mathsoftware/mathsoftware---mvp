0d56c67a680291bfc23182836c67d3488bb08514
Tobias Briones <tobiasbriones.dev@gmail.com>
2024-02-01 22:47:39 -0600

    Add info.tsx providing project versioning info

---
+++ math.software---mvp/src/info.tsx
@@ -0,0 +1,8 @@
+// Copyright (c) 2024 Tobias Briones. All rights reserved.
+// SPDX-License-Identifier: BSD-3-Clause
+// This file is part of https://github.com/mathsoftware/mathsoftware---mvp
+
+export const APP_SEMVER = "0.1.0-dev";
+export const APP_NAME = "Math.Software";
+export const APP_VERSION = `MVP v${ APP_SEMVER }`;
+export const APP_VERSION_LABEL = `${ APP_NAME } ${ APP_VERSION }`;
