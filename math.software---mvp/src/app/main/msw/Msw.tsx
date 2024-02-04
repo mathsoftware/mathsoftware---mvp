a41cffb76667c7e642b4ea9a4efceece0860774a
Tobias Briones <tobiasbriones.dev@gmail.com>
2024-02-04 17:29:37 -0600

    Add pure subheading to component Msw

--- math.software---mvp/src/app/main/msw/Msw.tsx
+++ math.software---mvp/src/app/main/msw/Msw.tsx
@@ -3,7 +3,8 @@
 // This file is part of https://github.com/mathsoftware/mathsoftware---mvp
 
 import mswIcon from "@app/assets/msw.svg";
-import { Heading } from "@app/main/Heading.tsx";
+import pureIcon from "@app/assets/pure.svg";
+import { Heading, SubHeading } from "@app/main/Heading.tsx";
 
 function Msw() {
     return <>
@@ -22,6 +23,37 @@ function Msw() {
                 Mathematical software is either <b>Pure</b>, <b>Applied</b>, or
                 <b> Special</b>.
             </p>
+
+            <section>
+                <SubHeading
+                    id="pure"
+                    title="Pure"
+                    icon={ pureIcon }
+                ></SubHeading>
+
+                <p>Mathematics</p>
+
+                <p>
+                    MSW supports mathematics similarly to functional languages
+                    supporting first-class functions, resulting in domain
+                    expertise
+                    with a <b>DSL (Domain-Specific Language)</b> engineered for
+                    expressive mathematics.
+                </p>
+
+                <p>
+                    First-Class mathematics enables state-of-the-art
+                    software that uncouples the math <b>domain</b> from
+                    the engineering <b>implementation</b>.
+                </p>
+
+                <p>
+                    The definition of mathematics via its DSL results
+                    in <a href="/">math.software </a>
+                    empowering <b>focused</b> mathematicians with no engineering
+                    requirements.
+                </p>
+            </section>
         </section>
     </>;
 }
